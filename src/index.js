0b9a6372b1c1a58e81fbf7a51e65961dc3eb3ffe
MarkoRRR <markowashere@gmail.com>
2019-06-12 00:39:37 +0200

    Completed Price History Feature

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
 import Chart from 'chart.js';
 
 window.onload = function () {
-  // getPriceHistory();
-  var price = getBitcoinPrice();
+  // Fetch the current bitcoin price
+  getBitcoinPrice();
+  // Fetch the last 31 days price history
+  getPriceHistory();
+};
 
+
+const axios = require('axios');
+
+function getBitcoinPrice(){
+  var price;
+  axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+  .then(function (response) {
+    // handle success
+    price = response.data.bpi.USD.rate;
+    price = price.split('.')[0];
+    console.log("Bitcoin Price: "+price+" $");
+    document.getElementById("btc-price").textContent = price+" $";
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+    return price;
+  });
+}
+
+function getPriceHistory(){
+  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?')
+  .then(function (response) {
+    // handle success
+    var dates = [];
+    var priceHistory = [];
+    var target = response.data.bpi;
+    console.log(target);
+    for (var k in target){
+      if (target.hasOwnProperty(k)) {
+        var date = k;
+        date = date.replace('-','');
+        date = date.replace('-','');
+
+        dates.push(date);
+        priceHistory.push(target[k]);
+        // console.log("Key is " + k + ", value is " + target[k]);
+       }
+    }
+    loadChart(dates, priceHistory);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+}
+
+function loadChart(dates, priceHistory){
   // Load the line chart with default mockup bitcoin values
   var myChart = new Chart(document.getElementById("myChart"), {
   type: 'line',
   data: {
-    labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+    labels: dates,
     datasets: [{
-        data: [4000,4500,5000,4400,7000,8800,7700,6900,7300, 7990],
-        label: "Last Month",
+        data: priceHistory,
+        label: "Past 31 Days",
         borderColor: "#3e95cd",
         fill: false
       }
@@ -23,47 +80,5 @@ window.onload = function () {
       text: 'Bitcoin Price History Powered by CoinDesk (USD)'
     }
   }
-});
-
-  console.log("Window Loaded");
-};
-
-const axios = require('axios');
-
-function getPriceHistory(){
-  // Make a request for a user with a given ID
-  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-02-01&end=2019-02-28')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
-}
-
-function getBitcoinPrice(){
-  var price;
-  // Make a request for a user with a given ID
-  axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-    price = response.data.bpi.USD.rate;
-    price = price.split('.')[0];
-    console.log("Bitcoin Price: "+price+" $");
-    document.getElementById("btc-price").textContent= price+" $";
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-    return price;
   });
 }
